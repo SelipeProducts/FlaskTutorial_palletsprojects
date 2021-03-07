6acc8b99b83bb77dfc7c2daf985396e5930a1e98
Cesar Felipe Lopez <cesarlopez200@gmail.com>
2021-03-07 21:47:41 +0000

    Covered logging through: debug, warning,& error

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 #Note: copy code from pt files and paste it into main to run
 
 #Book Notes:
+from flask import Flask
 import random
-from flask import Flask, flash, redirect, render_template, request, url_for
-
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 @app.route('/')
-def index():
-    return render_template('index_flash.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  error = None
-  if request.method == 'POST':
-    if request.form['username'] != 'admin' or request.form['password'] != 'secret': error = 'Invalid credentials'
-        
-    else:
-      flash('You were successfully logged in')
-      return redirect(url_for('index'))
-  return render_template('login_flash.html', error=error)
+def log():
+  app.logger.debug('A value for debugging')
+  app.logger.warning('A warning occurred (%d apples)', 69)
+  app.logger.error('An error occurred')
+  return "logger"
 
 if __name__ == '__main__':
    #app.run(debug = True)
